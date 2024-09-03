edfb243fd85da2121adebca003d38d41b873c3ae
Manav <manavmandli2990@gmail.com>
2024-09-03 23:06:21 +0530

    fix:changes

--- jewellery/jewellery/doctype/customer_bill_payment/customer_bill_payment.py
+++ jewellery/jewellery/doctype/customer_bill_payment/customer_bill_payment.py
@@ -1,9 +1,47 @@
 # Copyright (c) 2024, Manav Mandli and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
 
 class CustomerBillPayment(Document):
-	pass
+    def on_submit(self):
+        self.update_customer_payment_tracker()
+        self.create_payment_ledger()
+
+    def update_customer_payment_tracker(self):
+        if frappe.db.exists("Customer Payment Tracker", self.customer):
+            customer_payment_doc = frappe.get_doc(
+                "Customer Payment Tracker", self.customer
+            )
+            customer_payment_doc.pending_amount -= self.amount
+            customer_payment_doc.pending_metal -= self.weight
+            customer_payment_doc.updated_on = datetime.now()
+            customer_payment_doc.save()
+        else:
+            new_customer_payment_tracker = frappe.get_doc(
+                {
+                    "doctype": "Customer Payment Tracker",
+                    "customer": self.customer,
+                    "pending_amount": -self.amount,
+                    "pending_metal": -self.weight,
+                    "created_on": datetime.now(),
+                    "updated_on": datetime.now(),
+                }
+            )
+            new_customer_payment_tracker.insert()
+
+    def create_payment_ledger(self):
+        payment_ledger_doc = frappe.get_doc(
+            {
+                "doctype": "Payment Ledger",
+                "customer": self.customer,
+                "transaction_type": "Credit",
+                "pure_metal": self.weight,
+                "amount": self.amount,
+                "date": datetime.now(),
+            }
+        )
+        payment_ledger_doc.insert()
--- jewellery/jewellery/doctype/supplier_bill_payment/supplier_bill_payment.py
+++ jewellery/jewellery/doctype/supplier_bill_payment/supplier_bill_payment.py
@@ -1,9 +1,47 @@
 # Copyright (c) 2024, Manav Mandli and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
 
 class SupplierBillPayment(Document):
-	pass
+    def on_submit(self):
+        self.update_supplier_payment_tracker()
+        self.create_payment_ledger()
+
+    def update_supplier_payment_tracker(self):
+        if frappe.db.exists("Supplier Payment Tracker", self.supplier):
+            supplier_payment_doc = frappe.get_doc(
+                "Supplier Payment Tracker", self.supplier
+            )
+            supplier_payment_doc.pending_amount -= self.amount
+            supplier_payment_doc.pending_metal -= self.weight
+            supplier_payment_doc.updated_on = datetime.now()
+            supplier_payment_doc.save()
+        else:
+            new_supplier_payment_tracker = frappe.get_doc(
+                {
+                    "doctype": "Supplier Payment Tracker",
+                    "supplier": self.supplier,
+                    "pending_amount": -self.amount,
+                    "pending_metal": -self.weight,
+                    "created_on": datetime.now(),
+                    "updated_on": datetime.now(),
+                }
+            )
+            new_supplier_payment_tracker.insert()
+
+    def create_payment_ledger(self):
+        payment_ledger_doc = frappe.get_doc(
+            {
+                "doctype": "Payment Ledger",
+                "customer": self.supplier,
+                "transaction_type": "Credit",
+                "pure_metal": self.weight,
+                "amount": self.amount,
+                "date": datetime.now(),
+            }
+        )
+        payment_ledger_doc.insert()
